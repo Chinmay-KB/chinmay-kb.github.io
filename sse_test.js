88e9a515182f39fdedafbb4639c7ad61b52eabce
Chinmay <chinmay.kabi@live.com>
2022-04-16 16:43:45 +0530

    udao in website finally!

--- sse_test.js
+++ sse_test.js
@@ -1,9 +1,10 @@
-if(typeof(EventSource) !== "undefined") {
-  var source = new EventSource("http://128.199.24.73:3000/events");
-  source.onmessage = function(event) {
-    const obj = JSON.parse(event.data);
-    document.getElementById("sse-data").innerHTML= obj['data']+'</br>'+'Clients Live-'+obj['live_clients'];
-  };
+if (typeof(EventSource) !== "undefined") {
+    var source = new EventSource("https://udao.chinmaykabi.com/events");
+    source.onmessage = function(event) {
+        const obj = JSON.parse(event.data);
+        if (obj['data'] != null)
+            document.getElementById("sse-data").innerHTML = obj['data'] + ' | ' + +obj['live_clients'] + ' flying';
+    };
 } else {
-  document.getElementById("sse-data").innerHTML = "Sorry, your browser does not support server-sent events...";
+    document.getElementById("sse-data").innerHTML = "Sorry, your browser does not support server-sent events...";
 }
