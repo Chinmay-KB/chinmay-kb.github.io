44061b040ca30b2b1837a0b283808d04ac681d97
Chinmay <chinmay.kabi@live.com>
2022-04-23 02:29:36 +0530

    using emojis

--- sse_test.js
+++ sse_test.js
@@ -1,10 +1,23 @@
+// if (typeof(EventSource) !== "undefined") {
+//     var source = new EventSource("https://udao.chinmaykabi.com/events");
+//     source.onmessage = function(event) {
+//         const obj = JSON.parse(event.data);
+//         if (obj['data'] != null)
+//             document.getElementById("sse-data").innerHTML = obj['data'] + ' | ' + +obj['live_clients'] + ' flying';
+//     };
+// } else {
+//     document.getElementById("sse-data").innerHTML = "Sorry, your browser does not support server-sent events...";
+// }
+
 if (typeof(EventSource) !== "undefined") {
-    var source = new EventSource("https://udao.chinmaykabi.com/events");
+    var source = new EventSource("https://udao.chinmaykabi.com/emojis");
     source.onmessage = function(event) {
         const obj = JSON.parse(event.data);
         if (obj['data'] != null)
-            document.getElementById("sse-data").innerHTML = obj['data'] + ' | ' + +obj['live_clients'] + ' flying';
+            document.getElementById("sse-emoji").innerHTML = emoji[obj['data']] + ' | ' + obj['live_clients'] + ' being emoted';
     };
 } else {
     document.getElementById("sse-data").innerHTML = "Sorry, your browser does not support server-sent events...";
 }
+
+var emoji = ['🌽', '🍇', '🍌', '🍒', '🍕', '🍷', '🍭', '💖', '💩', '🐷', '🐸', '🐳', '🎃', '🎾', '🌈', '🍦', '💁', '🔥', '😁', '😱', '🌴', '👏', '💃'];
